4fa32a33f2ebb57e2a3e9d22a487e98b74136048
Erdem Karabekmez <erdemkarabekmez@gmail.com>
2025-07-28 21:20:18 +0300

    commit

--- src/test/java/com/remwaste/runners/CukeRunner.java
+++ src/test/java/com/remwaste/runners/CukeRunner.java
@@ -26,7 +26,7 @@ import org.junit.runner.RunWith;
         //glue = "com/remwaste/step_definitions",
         glue = "com.remwaste.step_definitions",
         dryRun = false,
-        tags = "@scenario_7"
+        tags = "@scenario_11"
 
 
 
